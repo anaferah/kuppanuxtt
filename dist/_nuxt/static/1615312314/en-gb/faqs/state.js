cec9b570cd2478dc839ec0bb61b3b7f79b3e0d04
anamaria <annah.mariyah'gmail.com>
2021-03-09 20:00:20 +0000

    adding dist folder
    adding dist folder

---
+++ dist/_nuxt/static/1615312314/en-gb/faqs/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615312314",layout:"default",error:null,state:{app:{scroll:{isScrolling:false,limit:{x:a,y:a},x:a,y:a}},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fen-gb\u002Ffaqs",config:{}}}(0));
